8368195d78c359e0f8a16196ef6ee4ef89efff7e
MAntoniS <sobota.antoni@gmail.com>
2020-12-20 21:52:42 -0800

    Update ProductBiggerThanNineThousand.java

--- 1 - 3/src/com/recruitment/first_task/ProductBiggerThanNineThousand.java
+++ 1 - 3/src/com/recruitment/first_task/ProductBiggerThanNineThousand.java
@@ -5,7 +5,7 @@ public class ProductBiggerThanNineThousand {
 
     public void rewriteArrayToList(int[] array){
         for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array.length; j++){
+            for (int j = i; j < array.length; j++){
                 int product = array[i] * array[j];
                 if (product > 9000) System.out.println
                         ("(" + i + ", " + j + ")");
